4fe0b1ff7ea5f66087e1f9e9ed40a2fb7823f42b
daniellemoses060 <142933424+daniellemoses060@users.noreply.github.com>
2025-04-22 23:46:11 +0200

    telegram_alerts.py

--- aviator-bot-railway/aviator_bot/telegram_alerts.py
+++ aviator-bot-railway/aviator_bot/telegram_alerts.py
@@ -1,1 +1,6 @@
+import telegram
 
+def send_telegram_alert(message):
+    bot = telegram.Bot(token="YOUR_BOT_TOKEN")
+    chat_id = "YOUR_CHAT_ID"
+    bot.send_message(chat_id=chat_id, text=message)
