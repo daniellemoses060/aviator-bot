02a3819cebc57e2fdbbe53980c60a76c8e5a1b0b
daniellemoses060 <142933424+daniellemoses060@users.noreply.github.com>
2025-04-22 23:46:47 +0200

    logger.py

--- aviator-bot-railway/aviator_bot/utils/logger.py
+++ aviator-bot-railway/aviator_bot/utils/logger.py
@@ -1,1 +1,9 @@
+import logging
 
+def log_info(message):
+    logging.basicConfig(level=logging.INFO)
+    logging.info(message)
+
+def log_error(message):
+    logging.basicConfig(level=logging.ERROR)
+    logging.error(message)
